228c58b9914dec653b8e5a64a0770b1561b1ccf1
richard-worth <58734385+richard-worth@users.noreply.github.com>
2020-04-06 16:57:14 +0100

    Add files via upload

---
+++ UK_Region.ps1
@@ -0,0 +1,25 @@
+﻿#variables
+#$regionalsettingsURL = "https://raw.githubusercontent.com/richard-worth/Azure/master/UK_Region.xml"
+$regionalsettingsURL = "https://nnuhfiles1918432868.file.core.windows.net/profiles/Settings/UK_Region.xml"
+$RegionalSettings = "C:\UK_Region.xml"
+
+
+#downdload regional settings file
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+
+
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+
+# Set languages/culture. Not needed perse.
+Set-WinSystemLocale en-GB
+Set-WinUserLanguageList -LanguageList en-GB -Force
+Set-Culture -CultureInfo en-GB
+Set-WinHomeLocation -GeoId 242
+Set-TimeZone -Name "GMT Standard Time"
+
+# restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
+Start-sleep -Seconds 40
+Restart-Computer
+
