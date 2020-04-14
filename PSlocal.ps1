e0a1da22cfef2b934bd815a15d34489e5833df97
richard-worth <58734385+richard-worth@users.noreply.github.com>
2020-04-14 09:05:06 +0100

    Add files via upload

---
+++ PSlocal.ps1
@@ -0,0 +1,15 @@
+﻿#variables
+$regionalsettingsURL = "https://raw.githubusercontent.com/richard-worth/Azure/master/GB_Region.xml"
+$RegionalSettings = "C:\UK_Region.xml"
+$LocalePSURL = "https://raw.githubusercontent.com/richard-worth/Azure/master/Locale.ps1"
+$LocalePS = "C:\Locale.ps1"
+
+#downnload regional settings file
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($LocalePSURL,$LocalePS)
+
+& $LocalePS
+
