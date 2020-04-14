4a259cbaf370dd6c95431c2eed552848909f298b
richard-worth <58734385+richard-worth@users.noreply.github.com>
2020-04-14 09:03:04 +0100

    Add files via upload

---
+++ Locale.ps1
@@ -0,0 +1,12 @@
+﻿# Set Locale, language etc.
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""   
+
+
+# Set languages/culture. Not needed perse.
+
+    Set-WinSystemLocale en-GB    
+    Set-WinUserLanguageList -LanguageList en-GB -Force    
+    Set-Culture -CultureInfo en-GB    
+    Set-WinHomeLocation -GeoId 242    
+    Set-TimeZone -Name "GMT Standard Time"    
+
---
+++ PSlocal2.ps1
@@ -0,0 +1,7 @@
+﻿
+
+$sysprep = 'C:\Windows\System32\Sysprep\Sysprep.exe'
+$arg = '/generalize /oobe /shutdown' # /quiet
+
+Start-Process -FilePath $sysprep -ArgumentList $arg -wait
+
